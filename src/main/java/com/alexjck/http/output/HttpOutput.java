be600b7db24d37b8bf7291cedceb486d7f2565ac
Alexander Campos <alexander.campos@mercadolibre.com>
2017-11-06 17:24:08 -0300

    Add field list

--- src/main/java/com/alexjck/http/output/HttpOutput.java
+++ src/main/java/com/alexjck/http/output/HttpOutput.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.configuration.fields.BooleanField;
 import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.ListField;
 import org.graylog2.plugin.configuration.fields.TextField;
@@ -22,105 +23,102 @@ import com.google.inject.assistedinject.Assisted;
 
 public class HttpOutput implements MessageOutput {
 
-    private static final String API_HOST = "api_host";
-    private static final String FIELD_LIST = "field_list";
+	private static final String API_HOST = "api_host";
+	private static final String FIELD_LIST = "field_list";
+	private static final String STREAM_BOOL = "should_send_stream";
 
-    private boolean running = true;
+	private boolean running = true;
 
-    private final Sender sender;
+	private final Sender sender;
 
-    @Inject
-    public HttpOutput(@Assisted Configuration configuration) throws MessageOutputConfigurationException {
-        // Check configuration.
-        if (!checkConfiguration(configuration)) {
-            throw new MessageOutputConfigurationException("Missing configuration.");
-        }
+	@Inject
+	public HttpOutput(@Assisted Configuration configuration) throws MessageOutputConfigurationException {
+		// Check configuration.
+		if (!checkConfiguration(configuration)) {
+			throw new MessageOutputConfigurationException("Missing configuration.");
+		}
 
-        // Set up sender.
-        sender = new HttpSender(
-                configuration.getString(API_HOST), configuration.getList(FIELD_LIST)
-        );
+		// Set up sender.
+		sender = new HttpSender(configuration.getString(API_HOST), configuration.getList(FIELD_LIST));
 
-        running = true;
-    }
+		running = true;
+	}
 
-    @Override
-    public void stop() {
-        sender.stop();
-        running = false;
-    }
+	@Override
+	public void stop() {
+		sender.stop();
+		running = false;
+	}
 
-    @Override
-    public boolean isRunning() {
-        return running;
-    }
+	@Override
+	public boolean isRunning() {
+		return running;
+	}
 
-    @Override
-    public void write(Message message) throws Exception {
-        if (message == null || message.getFields() == null || message.getFields().isEmpty()) {
-            return;
-        }
+	@Override
+	public void write(Message message) throws Exception {
+		if (message == null || message.getFields() == null || message.getFields().isEmpty()) {
+			return;
+		}
 
-        if(!sender.isInitialized()) {
-            sender.initialize();
-        }
+		if (!sender.isInitialized()) {
+			sender.initialize();
+		}
 
-        sender.send(message);
-    }
+		sender.send(message);
+	}
 
-    @Override
-    public void write(List<Message> list) throws Exception {
-        if (list == null) {
-            return;
-        }
+	@Override
+	public void write(List<Message> list) throws Exception {
+		if (list == null) {
+			return;
+		}
 
-        for(Message m : list) {
-            write(m);
-        }
-    }
+		for (Message m : list) {
+			write(m);
+		}
+	}
 
-    public boolean checkConfiguration(Configuration c) {
-        return c.stringIsSet(API_HOST);
-    }
+	public boolean checkConfiguration(Configuration c) {
+		return c.stringIsSet(API_HOST);
+	}
 
-    @FactoryClass
-    public interface Factory extends MessageOutput.Factory<HttpOutput> {
-        @Override
-        HttpOutput create(Stream stream, Configuration configuration);
+	@FactoryClass
+	public interface Factory extends MessageOutput.Factory<HttpOutput> {
+		@Override
+		HttpOutput create(Stream stream, Configuration configuration);
 
-        @Override
-        Config getConfig();
+		@Override
+		Config getConfig();
 
-        @Override
-        Descriptor getDescriptor();
-    }
+		@Override
+		Descriptor getDescriptor();
+	}
 
-    @ConfigClass
-    public static class Config extends MessageOutput.Config {
-        @Override
-        public ConfigurationRequest getRequestedConfiguration() {
-            final ConfigurationRequest configurationRequest = new ConfigurationRequest();
+	@ConfigClass
+	public static class Config extends MessageOutput.Config {
+		@Override
+		public ConfigurationRequest getRequestedConfiguration() {
+			final ConfigurationRequest configurationRequest = new ConfigurationRequest();
 
-            configurationRequest.addField(new TextField(
-                            API_HOST, "API Host", "",
-                            "Hostname or IP address of the API instance",
-                            ConfigurationField.Optional.NOT_OPTIONAL)
-            );
-            
-            configurationRequest.addField(new ListField(
-		                    FIELD_LIST, "Field list", Collections.emptyList(), Collections.emptyMap(),
-		                    "Field list to pass to the api",
-		                    ConfigurationField.Optional.NOT_OPTIONAL)
-            );
+			configurationRequest.addField(new TextField(API_HOST, "HTTP host", "", "Hostname or IP address",
+					ConfigurationField.Optional.NOT_OPTIONAL));
 
-            return configurationRequest;
-        }
-    }
+			configurationRequest.addField(new BooleanField(STREAM_BOOL, "Send stream in JSON?", false,
+					"Add stream field in the JSON request"));
 
-    public static class Descriptor extends MessageOutput.Descriptor {
-        public Descriptor() {
-            super("HTTP Output", false, "", "Writes messages to your API via HTTP.");
-        }
-    }
+			configurationRequest.addField(new ListField(FIELD_LIST, "Field list", Collections.emptyList(),
+					Collections.emptyMap(), "Field list to POST to the host as JSON",
+					ConfigurationField.Optional.NOT_OPTIONAL, ListField.Attribute.ALLOW_CREATE));
+
+			return configurationRequest;
+		}
+	}
+
+	public static class Descriptor extends MessageOutput.Descriptor {
+		public Descriptor() {
+			super("HTTP Output", false, "", "POST messages to your HOST via HTTP.");
+		}
+	}
 
 }
